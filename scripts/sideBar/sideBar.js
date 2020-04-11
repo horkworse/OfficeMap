96124d6e7334eadfe4d686ab9ad892ba6463b8dc
timurMikolenko <49361528+timurMikolenko@users.noreply.github.com>
2020-04-11 18:15:50 +0500

    !

---
+++ scripts/sideBar/sideBar.js
@@ -0,0 +1,31 @@
+let slide = () =>{
+    let user = document.querySelector('.user');
+    let close = document.querySelector('.sideBar__close');
+    let sideBar = document.querySelector('.sideBar__inner');
+    let slideLinks = document.querySelectorAll('.sideBar__links li');
+
+
+    user.addEventListener('click', () => {
+        newFunction(sideBar, slideLinks);
+    });  
+
+    //Повторение кода, знаю, потом подумаю как избавится, а может просто избавлюсь от крестика
+    close.addEventListener('click', () => {
+        newFunction(sideBar, slideLinks);
+    }); 
+
+}
+
+slide();
+
+function newFunction(sideBar, slideLinks) {
+    sideBar.classList.toggle('sideBar-active');
+    slideLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        }
+        else {
+            link.style.animation = `sideBarLinksFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+}
