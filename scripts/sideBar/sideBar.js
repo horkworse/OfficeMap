f077d8760a338d7f5c750f5f6f96cc5fa986884d
timurMikolenko <49361528+timurMikolenko@users.noreply.github.com>
2020-04-12 00:33:25 +0500

    /

--- scripts/sideBar/sideBar.js
+++ scripts/sideBar/sideBar.js
@@ -1,18 +1,20 @@
 let slide = () =>{
     let user = document.querySelector('.user');
     let close = document.querySelector('.sideBar__close');
-    // let cont = document.querySelector('.nav');
     let sideBar = document.querySelector('.sideBar__inner');
     let slideLinks = document.querySelectorAll('.sideBar__links li');
 
+    let cont = document.querySelector('.content');
+
 
     user.addEventListener('click', () => {
-        // cont.classList.toggle('nav-active');
+        cont.classList.toggle('content-active');
         slideFunction(sideBar, slideLinks);
     });  
 
     //Повторение кода, знаю, потом подумаю как избавится, а может просто избавлюсь от крестика
     close.addEventListener('click', () => {
+        cont.classList.toggle('content-active');
         slideFunction(sideBar, slideLinks);
     }); 
 
